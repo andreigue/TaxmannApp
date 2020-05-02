1eb02e7699a3a752a059b972f1f5972a064be623
andreigue <andreiguevorkian@mail.mcgill.ca>
2020-05-02 14:04:15 -0400

    registration page finish impleentation

--- app/src/main/java/com/example/taxmann/RegistrationActivity.java
+++ app/src/main/java/com/example/taxmann/RegistrationActivity.java
@@ -2,6 +2,7 @@ package com.example.taxmann;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,7 +25,17 @@ public class RegistrationActivity extends AppCompatActivity {
         regButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                validate();
+                if(validate()){
+                    //uplaod data to the db
+                }
+            }
+        });
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+
             }
         });
     }
@@ -48,7 +59,7 @@ public class RegistrationActivity extends AppCompatActivity {
         }else{
             result=true;
         }
-        
+
         return result;
     }
 }
