dfc4df1e8c12a7ef5082ad6fc809605372798bd1
andreigue <andreiguevorkian@mail.mcgill.ca>
2020-05-02 13:49:26 -0400

    adding implementation for registration page

--- app/src/main/java/com/example/taxmann/RegistrationActivity.java
+++ app/src/main/java/com/example/taxmann/RegistrationActivity.java
@@ -3,12 +3,52 @@ package com.example.taxmann;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText userName, userEmail, userPassword; //not related to the IDs
+    private Button regButton;
+    private TextView userLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        setupUIViews();
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate();
+            }
+        });
+    }
+
+    private void setupUIViews(){
+        userName = (EditText)findViewById(R.id.etName);
+        userEmail = (EditText)findViewById(R.id.etUserEmail);
+        userPassword = (EditText)findViewById(R.id.etUserPassword);
+        regButton = (Button)findViewById(R.id.btnRegister);
+        userLogin = (TextView)findViewById(R.id.tvUserLogin);
+    }
+
+    private Boolean validate(){
+        Boolean result = false;
+        String name = userName.getText().toString();
+        String pass = userPassword.getText().toString();
+        String email = userEmail.getText().toString();
+
+        if (name.isEmpty() || pass.isEmpty() || email.isEmpty()){
+            Toast.makeText(this, "Please Enter all the details", Toast.LENGTH_SHORT).show();
+        }else{
+            result=true;
+        }
+        
+        return result;
     }
 }
