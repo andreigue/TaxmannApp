49665b38fb85c631a3b516c1fa00a684a81ffdec
andrei <andrei.guevorkian@hotmail.com>
2020-05-17 22:29:03 -0400

    navigation drawer using fragment: functional

--- app/src/main/java/com/example/taxmann/MainActivity.java
+++ app/src/main/java/com/example/taxmann/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.taxmann;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, NewsRoomFragment.onFragmentBtnSelected {
     private DrawerLayout drawer;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +30,22 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+        if(savedInstanceState == null){
+            //load default fragment
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.fragment_container, new NewsRoomFragment());
+            fragmentTransaction.commit();
+            navigationView.setCheckedItem(R.id.nav_newsroom);
+        }
     }
 
     @Override
@@ -36,5 +55,38 @@ public class MainActivity extends AppCompatActivity {
         }else{
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        drawer.closeDrawer(GravityCompat.START);
+        if(item.getItemId() == R.id.nav_newsroom){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new NewsRoomFragment());
+            fragmentTransaction.commit();
+        }if(item.getItemId() == R.id.nav_chat){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new ChatFragment());
+            fragmentTransaction.commit();
+        }if(item.getItemId() == R.id.nav_profile){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new ProfileFragment());
+            fragmentTransaction.commit();
+        }
+        if(item.getItemId() == R.id.nav_logout){
+            Toast.makeText(this, "Logout button clicked.", Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId() == R.id.nav_share){
+            Toast.makeText(this, "Share button clicked.", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+    @Override
+    public void onBtnSelected() {
+
     }
 }
---
+++ app/src/main/java/com/example/taxmann/NewsRoomFragment.java
@@ -0,0 +1,35 @@
+package com.example.taxmann;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NewsRoomFragment extends Fragment {
+    private onFragmentBtnSelected listener;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_newsroom, container, false);
+        return view;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if(context instanceof onFragmentBtnSelected){
+            listener= (onFragmentBtnSelected) context;
+        }else{
+            throw new ClassCastException(context.toString() + "must implement listener");
+        }
+;    }
+
+    public interface onFragmentBtnSelected{
+        public void onBtnSelected();
+    }
+}
